28002d34d3c87b725089ed7586fc00d5174fca18
Ayo Abimbola <ayoinspired@gmail.com>
2021-05-23 21:37:32 +0100

    Setting up nav, other sections, and mobile menu.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,25 @@
-import React from "react";
-import styled from "styled-components"
+import React, { useState } from "react";
+import styled from "styled-components";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
 
 function Header() {
+	const [burgerStatus, setBurgerStatus] = useState(false);
+
 	return (
 		<Container>
 			<a>
 				<img src="/images/logo.svg" alt="" />
 			</a>
+
+			{/* {window.addEventListener("scroll", function () {
+				if (window.scrollY > 200) {
+					Menu.classList.add("redBg");
+					console.log("add background to fixed");
+				} else {
+					console.log("less than 200");
+				}
+			})} */}
 
 			<Menu>
 				<a href="#">Model S</a>
@@ -18,8 +31,35 @@ function Header() {
 			<RightMenu>
 				<a href="#">Shop</a>
 				<a href="#">Account</a>
+				<CustomMenu onClick={() => setBurgerStatus(true)} />
 			</RightMenu>
 
+			<BurgerNav show={burgerStatus}>
+				<CloseWrapper>
+					<CustomClose onClick={() => setBurgerStatus(false)} />
+				</CloseWrapper>
+				<li>
+					<a href="#">Existing Inventory</a>
+				</li>
+				<li>
+					<a href="#">Used Inventory</a>
+				</li>
+				<li>
+					<a href="#">Trade-in</a>
+				</li>
+				<li>
+					<a href="#">Cyber Truck</a>
+				</li>
+				<li>
+					<a href="#">Roadster</a>
+				</li>
+				<li>
+					<a href="#">Existing Inventory</a>
+				</li>
+				<li>
+					<a href="#">Existing Inventory</a>
+				</li>
+			</BurgerNav>
 		</Container>
 	);
 }
@@ -27,41 +67,82 @@ function Header() {
 export default Header;
 
 const Container = styled.div`
-min-height: 60px;
-position: fixed;
-display: flex;
-align-items: center;
-padding: 0 20px;
-top: 0;
-left: 0;
-right: 0;
-`
+	min-height: 60px;
+	position: fixed;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: 0 20px;
+	top: 0;
+	left: 0;
+	right: 0;
+	z-index: 1;
+`;
 
 const Menu = styled.div`
-display: flex;
-align-items: center;
-flex: 1;
-justify-content: center;
+	display: flex;
+	align-items: center;
+	flex: 1;
+	justify-content: center;
 
-a {
-	font-weight: 600;
-	text-transform: uppercase;
-	padding: 0 8px;
-	flex-wrap: no-wrap;
-	white-space: nowrap;
-}
-`
+	a {
+		font-weight: 600;
+		text-transform: uppercase;
+		padding: 0 8px;
+		flex-wrap: no-wrap;
+		white-space: nowrap;
+	}
+
+	@media (max-width: 768px) {
+		display: none;
+	}
+`;
 
 const RightMenu = styled.div`
-display: flex;
-align-items: center;
-justify-content: center;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 
 	a {
 		font-weight: 600;
 		text-transform: uppercase;
 		text-align: center;
 		margin-right: 8px;
-
 	}
+`;
+
+const CustomMenu = styled(MenuIcon)`
+	cursor: pointer;
+`;
+
+const BurgerNav = styled.div`
+	position: fixed;
+	top: 0;
+	bottom: 0;
+	right: 0;
+	background-color: #fff;
+	width: 300px;
+	z-index: 16;
+	list-style: none;
+	padding: 20px;
+	text-align: left;
+	display: flex;
+	flex-direction: column;
+	transition: transform 0.3s cubic-bezier(1, 0, 0, 1);
+	transform: ${(props) => (props.show ? "translateX(0)" : "translateX(100%)")};
+
+	li {
+		padding: 15px 0;
+		border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+		font-weight: 600;
+	}
+`;
+
+const CustomClose = styled(CloseIcon)`
+	cursor: pointer;
+`;
+
+const CloseWrapper = styled.div`
+	display: flex;
+	justify-content: flex-end;
 `;
